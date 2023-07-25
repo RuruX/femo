Fixed the aero loads mapping; Convert all of the units to meters for the PAV example.

examples/test_shell_m3l/shell_module.py
@@ -5,6 +5,7 @@ from lsdo_modules.module_csdl.module_csdl import ModuleCSDL
 from lsdo_modules.module.module import Module
 from shell_pde import ShellPDE, ShellModule, NodalMap
 import numpy as np
+from scipy.sparse.linalg import spsolve
 
 from typing import Tuple, Dict
 """
@@ -171,13 +172,15 @@ class RMShellForces(m3l.ExplicitOperation):
                             shape=output_shape, operation=self)
         return shell_forces
 
+
     def fmap(self, mesh, oml):
-        # Fs = W*Fp
-        G_mat = NodalMap(mesh, oml, RBF_width_par=2.0,
+        G_mat = NodalMap(mesh, oml, RBF_width_par=4.,
                             column_scaling_vec=self.pde.bf_sup_sizes)
-        weights = G_mat.map.T
-        # print("Force map shape:", weights.shape)
+        rhs_mats = G_mat.map.T
+        mat_f_sp = self.pde.compute_sparse_mass_matrix()
+        weights = spsolve(mat_f_sp, rhs_mats)
         return weights
+
 
 class RMShellNodalDisplacements(m3l.ExplicitOperation):
     def initialize(self, kwargs):
@@ -253,7 +256,6 @@ class RMShellNodalDisplacements(m3l.ExplicitOperation):
         G_mat = NodalMap(mesh, oml, RBF_width_par=2.0,
                             column_scaling_vec=self.pde.bf_sup_sizes)
         weights = G_mat.map
-        # print("Disp map shape:", weights.shape)
         return weights
 
 

examples/test_shell_m3l/shell_pde.py
@@ -377,6 +377,7 @@ class ShellPDE(object):
         self.VF = VectorFunctionSpace(mesh, ("CG", 1))
         self.bf_sup_sizes = assemble_vector(
                 form(TestFunction(self.VF.sub(0).collapse()[0])*dx)).getArray()
+        # self.bf_sup_sizes = np.ones_like(self.bf_sup_sizes)
 
     def compute_alpha(self):
         h_mesh = ufl.CellDiameter(self.mesh)
@@ -430,7 +431,6 @@ class ShellPDE(object):
 
     def total_force(self,f):
         fz = f.sub(2).collapse()
-        print(fz.x.array)
         return fz*dx
 
     def mass(self,h,rho):
@@ -492,6 +492,23 @@ class ShellPDE(object):
         disp_extraction_mats = sp.vstack(deriv_us_to_ua_coord_list)
         # print(disp_extraction_mats.shape)
         return disp_extraction_mats
+
+    def compute_sparse_mass_matrix(self):
+        # functions used to assemble FEA mass matrix
+        f_trial = TrialFunction(self.VT)
+        f_test = TestFunction(self.VT)
+
+        # assemble PETSc mass matrix
+        Mat_f = assemble_matrix(form(inner(f_test, f_trial)*dx))
+        Mat_f.assemble()
+
+        # convert mass matrix to sparse Python array
+        Mat_f_csr = Mat_f.getValuesCSR()
+        Mat_f_sp = sp.csr_matrix((Mat_f_csr[2], Mat_f_csr[1], Mat_f_csr[0]))
+
+        # eliminate zeros that are present in mass matrix
+        Mat_f_sp.eliminate_zeros()
+        return Mat_f_sp
 
     def construct_disp_extraction_mats(self):
         # first we construct the extraction matrix for all displacements