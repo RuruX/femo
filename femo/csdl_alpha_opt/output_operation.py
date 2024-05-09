e50cfab2be3b11cdf77f5e504ab9e8d8adc390d6
Rurux <xiangrurose@gmail.com>
2024-05-09 16:02:18 -0700

    Initial commit of the csdl_alpha branch.

---
+++ femo/csdl_alpha_opt/output_operation.py
@@ -0,0 +1,130 @@
+from femo.fea.fea_dolfinx import FEA, update, assemble, computePartials, getFuncArray
+import csdl_alpha as csdl
+import numpy as np
+
+
+class OutputOperation(csdl.CustomExplicitOperation):
+    """
+    input: input/state variables
+    output: output
+    """
+
+    def __init__(self, fea, args_name_list, output_name):
+        super().__init__()
+
+        # define any checks for the parameters
+        csdl.check_parameter(fea, "fea", types=FEA)
+        csdl.check_parameter(args_name_list, "args_name_dict", types=list)
+        csdl.check_parameter(output_name, "output_name", types=str)
+
+        args_dict = dict()
+        for arg_name in args_name_list:
+            if arg_name in fea.inputs_dict:
+                args_dict[arg_name] = fea.inputs_dict[arg_name]
+            elif arg_name in fea.states_dict:
+                args_dict[arg_name] = fea.states_dict[arg_name]
+
+        # assign parameters to the class
+        self.fea = fea
+        self.args_dict = args_dict
+        self.output_name = output_name
+        self.fea_output = fea.outputs_dict[output_name]
+
+    def evaluate(self, inputs: dict):
+        # assign method inputs to input dictionary
+        for arg_name in self.args_dict:
+            if arg_name in inputs:
+                self.declare_input(arg_name, inputs[arg_name])
+            else:
+                raise ValueError(f"Variable {arg_name} not found in the FEA model.")
+
+        # declare output variables
+        output = self.create_output(self.output_name, (1,))
+
+        # declare any derivative parameters
+        self.declare_derivative_parameters(self.output_name, "*", dependent=False)
+        output.add_name(self.output_name)
+        return output
+
+    def compute(self, input_vals, output_vals):
+        for arg_name in input_vals:
+            arg = self.args_dict[arg_name]
+            update(arg["function"], input_vals[arg_name])
+
+        output_vals[self.output_name] = assemble(self.fea_output["form"])
+
+    def compute_derivatives(self, input_vals, output_vals, derivatives):
+        for arg_name in input_vals:
+            arg = self.args_dict[arg_name]
+            update(arg["function"], input_vals[arg_name])
+
+        for arg_name in input_vals:
+            derivatives[self.output_name, arg_name] = assemble(
+                computePartials(
+                    self.fea_output["form"], self.args_dict[arg_name]["function"]
+                ),
+                dim=self.output_dim + 1,
+            )
+
+
+class OutputFieldOperation(csdl.CustomExplicitOperation):
+    """
+    input: input/state variables
+    output: output
+    """
+
+    def __init__(self, fea, args_name_list, output_name):
+        super().__init__()
+
+        # define any checks for the parameters
+        csdl.check_parameter(fea, "fea", types=FEA)
+        csdl.check_parameter(args_name_list, "args_name_dict", types=list)
+        csdl.check_parameter(output_name, "output_name", types=str)
+
+        args_dict = dict()
+        for arg_name in args_name_list:
+            if arg_name in fea.inputs_dict:
+                args_dict[arg_name] = fea.inputs_dict[arg_name]
+            elif arg_name in fea.states_dict:
+                args_dict[arg_name] = fea.states_dict[arg_name]
+
+        # assign parameters to the class
+        self.fea = fea
+        self.args_dict = args_dict
+        self.output_name = output_name
+        self.fea_output = fea.outputs_dict[output_name]
+
+    def evaluate(self, inputs: csdl.VariableGroup):
+        # assign method inputs to input dictionary
+        for arg_name in self.args_dict:
+            arg = self.args_dict[arg_name]
+            self.declare_input(arg_name, inputs[arg_name])
+
+        # declare output variables
+        output = self.create_output(self.output_name, (1,))
+
+        # declare any derivative parameters
+        self.declare_derivative_parameters(self.output_name, "*", dependent=False)
+        output.add_name(self.output_name)
+        return output
+
+    def compute(self, input_vals, output_vals):
+        for arg_name in input_vals:
+            arg = self.args_dict[arg_name]
+            update(arg["function"], input_vals[arg_name])
+
+        output_vals[self.output_name] = assemble(self.fea_output["form"])
+
+    def compute_derivatives(self, input_vals, output_vals, derivatives):
+        for arg_name in input_vals:
+            arg = self.args_dict[arg_name]
+            update(arg["function"], input_vals[arg_name])
+
+        for arg_name in input_vals:
+            derivatives[self.output_name, arg_name] = assemble(
+                computePartials(
+                    self.fea_output["form"], self.args_dict[arg_name]["function"]
+                ),
+                dim=self.output_dim + 1,
+            )
+
---
+++ femo/csdl_alpha_opt/state_operation.py
@@ -0,0 +1,129 @@
+from femo.fea.fea_dolfinx import FEA, update, getFuncArray
+import csdl_alpha as csdl
+
+
+class StateOperation(csdl.CustomExplicitOperation):
+    """
+    input: input variable
+    output: state
+    """
+
+    def __init__(self, fea, args_name_list, state_name, debug_mode=False):
+        super().__init__()
+        """
+        Initialize the StateOperation object.
+
+        Parameters:
+        ----------
+        fea (FEA): An instance of the FEA class.
+        args_name_list (list): A list of argument names.
+        state_name (str): The name of the state.
+        debug_mode (bool, optional): If set to True, the debug mode is enabled. Defaults to False.
+        """
+        # define any checks for the parameters
+        csdl.check_parameter(fea, "fea", types=FEA)
+        csdl.check_parameter(args_name_list, "args_name_list", types=list)
+        csdl.check_parameter(state_name, "state_name", types=str)
+
+        args_dict = dict()
+        for arg_name in args_name_list:
+            args_dict[arg_name] = fea.inputs_dict[arg_name]
+
+        # assign parameters to the class
+        self.fea = fea
+        self.args_dict = args_dict
+        self.state_name = state_name
+        self.debug_mode = debug_mode
+
+    def evaluate(self, inputs: dict):
+        """
+
+        Evaluate the state operation
+
+        parameters:
+        ----------
+        inputs (dict): A dictionary of input variables (csdl.Variable).
+
+        returns:
+        --------
+        state (csdl.Variable): The state variable.
+        """
+
+        if self.debug_mode is True:
+            print("=" * 15 + str(self.state_name) + "=" * 15)
+            print("CSDL: Running evaluate()...")
+            print("=" * 40)
+
+        # assign method inputs to input dictionary
+        for input_name in self.args_dict:
+            self.declare_input(input_name, inputs[input_name])
+
+        # declare output variables
+        self.fea_state = self.fea.states_dict[self.state_name]
+        state = self.create_output(
+            self.state_name,
+            shape=(self.fea_state["shape"],),
+        )
+        state.add_name(self.state_name)
+
+        # declare any derivative parameters
+        self.declare_derivative_parameters(self.state_name, "*", dependent=False)
+
+        return state
+
+    def compute(self, input_vals, output_vals):
+        """
+        Compute the state operation
+
+        parameters:
+        ----------
+        input_vals (dict): A dictionary of input variable values (numpy.ndarray).
+        output_vals (dict): A dictionary of output variable values (numpy.ndarray).
+        """
+
+        if self.debug_mode is True:
+            print("=" * 15 + str(self.state_name) + "=" * 15)
+            print("CSDL: Running compute()...")
+            print("=" * 40)
+
+        self.solve_residual_equations(input_vals)
+        output_vals[self.state_name] = getFuncArray(self.fea_state["function"])
+        
+
+    def solve_residual_equations(self, input_vals):
+        """
+        Solve the residual equations using FEMO solver.
+
+        parameters:
+        ----------
+        input_vals (dict): A dictionary of input variable values (numpy.ndarray).
+
+        """
+        if self.debug_mode is True:
+            print("=" * 15 + str(self.state_name) + "=" * 15)
+            print("CSDL: Running solve_residual_equations()...")
+            print("=" * 40)
+
+        # update the input values in the FEA model
+        self.fea.opt_iter += 1
+        for arg_name in input_vals:
+            arg = self.args_dict[arg_name]
+            update(arg["function"], input_vals[arg_name])
+            if arg["record"]:
+                arg["recorder"].write_function(arg["function"], self.fea.opt_iter)
+        
+        # solve the residual equation
+        self.fea.solve(
+            self.fea_state["residual_form"], self.fea_state["function"], self.fea.bc
+        )
+        
+        # record the function values in XDMF files
+        if self.fea.record:
+            if self.fea_state["function"].function_space.num_sub_spaces > 1:
+                u_mid, _ = self.fea_state["function"].split()
+                self.fea_state["recorder"].write_function(u_mid, self.fea.opt_iter)
+
+            else:
+                self.fea_state["recorder"].write_function(
+                    self.fea_state["function"], self.fea.opt_iter
+                )
